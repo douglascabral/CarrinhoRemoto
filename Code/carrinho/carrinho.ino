061eb05454d5b93fdee3bbb512b45c5a04ff7adc
Douglas Cabral <douglascabral@users.noreply.github.com>
2022-05-01 15:14:41 -0300

    Testes do servo motor da direção e dos analógicos do joystick

--- Code/carrinho/carrinho.ino
+++ Code/carrinho/carrinho.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <ESP32Servo.h>
 
+int PIN_SERVO_MOTOR_DIRECAO = 13;
+
+Servo motorDirecao;
+int MOTOR_DIRECAO_INITIAL_POSITION = 90;
+int MOTOR_DIRECAO_MAX_LEFT_POSITION = 135;
+int MOTOR_DIRECAO_MAX_RIGHT_POSITION = 45;
+
+void setup() {
+  motorDirecao.setPeriodHertz(50);
+  motorDirecao.attach(PIN_SERVO_MOTOR_DIRECAO, 500, 2400);
+  motorDirecao.write(MOTOR_DIRECAO_INITIAL_POSITION);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
--- Code/controle/controle.ino
+++ Code/controle/controle.ino
@@ -1,9 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
+int PIN_JOYSTICK_EIXO_Y = 36; // Pino número 3 da placa
+int PIN_JOYSTICK_EIXO_X = 39; // Pino número 4 da placa
 
+void setup() {
+  pinMode(PIN_JOYSTICK_EIXO_Y, INPUT);
+  pinMode(PIN_JOYSTICK_EIXO_X, INPUT);    
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int x = analogRead(PIN_JOYSTICK_EIXO_X);
+  int y = analogRead(PIN_JOYSTICK_EIXO_Y);
 
+  x = map(x, 0, 4095, 0, 255);
+  y = map(y, 0, 4095, 0, 255);
+  
+  Serial.print("X: ");
+  Serial.println(x);
+
+  Serial.print("Y: ");
+  Serial.println(y);
 }
